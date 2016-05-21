200cd003510bdde26ff4bc20033484c2e1ddebc4
Arthur Lapraye <arthurlapraye@yahoo.fr>
2016-05-21 18:20:45 +0200

    comparaison de const
    Fonction de comparaison entre les constituants de deux arbres

--- evaluation/eval.py
+++ evaluation/eval.py
@@ -3,6 +3,7 @@
 #Arthur Lapraye - 2016
 
 def tokenize(string):
+	""" Tokenise une S-expression """
 	return string.replace('(',' ( ').replace(')',' ) ').split()
 
 def readtree(tokens):
@@ -52,37 +53,52 @@ def defoliate(tree):
 	
 	return newtree
 
-def getspans(tree,offset=0):
+def getspans(tree):
 	"""
 	Prend en entrée un arbre de constituants tel que produit par readtree et une position de départ dans la phrase, par défaut 0
 	Renvoie une liste de tuples contenant l'étiquette du constituant et son span dans la phrase
 	"""
-	spans=list()
-	beginoffset=offset
-	# print tree
-	for elem in tree[1:]:
-		
-		if isinstance(elem,list):
-			sp,of= getspans(elem,offset)
-			spans += sp
-			offset = of
-		else:
-			spans.append( (elem,offset,offset+1) )
-			offset += 1
+	def getsp(tree,offset):
+		spans=list()
+		beginoffset=offset
+		# print tree
+		for elem in tree[1:]:
+			if isinstance(elem,list):
+				sp,of= getsp(elem,offset)
+				spans += sp
+				offset = of
+			else:
+				spans.append( (elem,offset,offset+1) )
+				offset += 1
 		
 	
-	spans.append((tree[0],beginoffset,offset))
+		spans.append((tree[0],beginoffset,offset))
 	
-	return spans,offset
+		return spans,offset
+	
+	return getsp(tree,0)[0]
 
-def parseval(gold, pred):
+def goodconst(tree1,tree2):
+	"""
+	Compare les constituants de deux arbres en fonction de leurs constituants communs
+	Deux constituants sont identiques s'ils ont le même nom et le même span.
+	Les constituants communs aux deux arbres sont comptabilisés dans la variable correct
+	Les constituants de tree1 absent de tree2 sont comptabilisés dans err1
+	Les constituants de tree2 absents de tree1 sont comptabilisés dans err2
+	"""
+	correct,err1,err2=0.0,0.0,0.0
+	resultat=getspans(tree2)
 	
-	for elem in zip(sorted(gold,key=lambda (x,y,z) : x)):
-		pass
-		"""
-			dictionnaire de sets de spans par nom de syntagme ?
-			sinon calcul inter, puis gold - inter et pred - inter
-		"""
+	for elem in getspans(tree1):
+		if elem in resultat:
+			correct += 1
+			resultat.pop(resultat.index(elem))
+		else:
+			err1 += 1
+	
+	err2+=len(resultat)
+	
+	return correct,err1,err2
 
 
 if __name__ == "__main__":
@@ -98,13 +114,13 @@ if __name__ == "__main__":
 		line=line.decode("utf-8")
 		tree=readtree(tokenize(line))[0]
 		leaves=getleaves(tree)
-		spans=getspans(defoliate(tree))[0]
-		
+		spans=getspans(defoliate(tree))
 		print
 		print leaves
 		print tree
 		print defoliate(tree)
 		print getleaves(defoliate(tree))
+		print goodconst(tree,defoliate(tree))
 		for const,i,j in sorted(spans,key=lambda (x,y,z) : z):
 			print const.encode("utf-8"),i,leaves[i:j],j
 
