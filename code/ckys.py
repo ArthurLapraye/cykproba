428abec251622f17faa83ad6584393ea6cc249f3
Krolov18 <korantin.leveque@gmail.com>
2016-06-17 22:47:26 +0200

    mise à jour cky

--- code/ckys.py
+++ code/ckys.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
 # coding: utf8
 
-import grammaires
-import productions
-import nonterminal
-from yaml.representer import Representer
-import yaml
+import collections
+import fractions
 
 
 
 class CKY_TITOV(object):
     def __init__(self):
-        self.chart = dict()
+        self.chart = collections.defaultdict(lambda : collections.defaultdict(int))
 
     def get_sent_sommet(self, phrase):
         for x in self.grammaire['axiome']:
@@ -21,52 +18,18 @@ class CKY_TITOV(object):
                 print("La phrase n'est pas reconnue par la grammaire")
             return (sent_sommet, (0, len(phrase)))
 
-    def load_grammar(self, grammaire):
-        print("Chargement de la grammaire: \n")
-        if not issubclass(type(grammaire), grammaires.Grammairehorscontexteprobabilistecnf):
-            raise TypeError('CKY ne peut fonctionner qu\'avec une grammaire hors contexte mise sous forme normale de chomsky')
-        else:
-            self.grammaire = grammaire
-
-    def predit(self, phrase):
+    def predit(self, phrase, cnf):
         print('Prédiction: ')
-        self.span = (x for x,y in enumerate(phrase, 1))
-        self.initialise(phrase)
-        self.remplit(phrase)
 
-        return self.get_arbre(self.get_sent_sommet(phrase))
+        chart = collections.defaultdict(lambda : collections.defaultdict(int))
 
-    def initialise(self, phrase):
-        print('Initialisation de la charte: ')
-        s = next(self.span)
-        for (t, mot) in enumerate(phrase):
-            self.chart[(s, t)] = {}
-            for terminal in self.grammaire.terminals:
-                self.chart[(s, t)][terminal] = 0
-                for lexicale in productions.Production.subset_productions(self.grammaire.productions, productions.Productionhorscontexte1lexicaleprobabilisee, terminal):
-                    self.chart[(s, t)][terminal] = (lexicale.proba, ((t, s),))
+        return self.get_sommet(self.remplit(phrase, cnf, self.initialise(phrase, cnf)), phrase)
+        # return get_arbre(get_sommet(remplit(phrase, cnf, initialise(phrase, cnf, chart))))
 
-    def remplit(self, phrase):
-        print('Remplissage de la charte: ')
-        for max in self.span:
-            for min in range(max-2, 0, -1):
-                self.chart[(min, max)] = dict()
-                for nonterminal in self.grammaire["nonterminals"]:
-                    best = 0
-                    self.chart[(min, max)][nonterminal] = (0,)
-                    for binaire in productions.Production.subset_productions(
-                            self.grammaire.productions,
-                            productions.Productionhorscontexte2binaireprobabilisee, nonterminal):
-                        for mid in range(min + 1, max - 1):
-                            t1 = self.chart[(min, mid)][binaire[1][0]]
-                            t2 = self.chart[(mid, max)][binaire[1][1]]
-                            if (t1[0] == 0) or (t2[0] == 0):
-                                candidate = 0
-                            else:
-                                candidate = t1[0] * t2[0] * binaire.proba
-                            if candidate > best:
-                                best = (candidate, ((min, mid, binaire[1][0]), (mid, max, binaire[1][1])))
-                    self.chart[(min, max)][nonterminal] = best
+    def get_sommet(self, chart, phrase):
+        for x in list(chart[(len(phrase), 0)]):
+            if (x == "SENT") or "SENT" in x:
+                print(chart[(len(phrase), 0)][x])
 
     def get_arbre(self, sent_sommet):
         pattern = '({tete} {enfants})'
@@ -80,20 +43,68 @@ class CKY_TITOV(object):
                     enfants=" ".join([self.get_arbre(child) for child in sent_sommet[1]])
                 )
 
+    def initialise(self, phrase, cnf):
+        print('Initialisation de la charte: ')
+
+        chart = collections.defaultdict(lambda: collections.defaultdict(int))
+
+        (terminaux, nonterminaux, regles) = cnf
+
+        s = 1
+
+        for (t, _) in enumerate(phrase):
+            for lhs in list(regles):
+                for rhs in list(regles.get(lhs)):
+                    if len(rhs) == 1:
+                        chart[(s, t)][lhs] = regles[lhs][rhs[0]]
+                        # chart[(s, t)][lhs] = (regles[lhs][rhs[0]], ((t, s),))
+        print('Chart initialisée')
+        return chart
+
+    def remplit(self, phrase, cnf, chart):
+        print('Remplissage de la charte: ')
+
+        span = (x for (x, y) in enumerate(phrase[1:], 2))
+
+        (terminaux, nonterminaux, regles) = cnf
+
+        for max in span:
+            for min in range(max-2, 0, -1):
+                print(min, max)
+                for nonterminal in nonterminaux:
+                    best = 0
+                    for rhs in list(regles[nonterminal]):
+                        if len(rhs) == 2:
+                            for mid in range(min + 1, max - 1):
+                                t1 = chart[(min, mid)][rhs[0]]
+                                t2 = chart[(mid, max)][rhs[1]]
+                                candidate = t1 * t2 * regles[nonterminal][rhs]
+                                if candidate > best:
+                                    best = candidate
+                                    # best = (candidate, ((min, mid, rhs[0]), (mid, max, rhs[1])))
+                            chart[(min, max)][nonterminal] = best
+        print('Chart remplie')
+        return chart
+
+def defaultdictmaker():
+    return collections.defaultdict(fractions.Fraction)
+
 def main():
     import pickle
-    from nonterminal import nonterminal_representer
+    import evaluation
+    import sys, codecs
 
-    yaml.add_representer(InfiniteDict, Representer.represent_dict)
-    yaml.add_representer(nonterminal.Nonterminal, nonterminal_representer)
+    cnf = pickle.load(open('grammaire.pickle', 'rb'))
 
-    cky = CKY_TITOV()
-    grammaire = pickle.load(open("grammaire_200_pcnf.pickle", 'rb'))
-    cky.load_grammar(grammaire=grammaire)
-    b = "Cet été".split(' ')
-    # for p in grammaire.productions:
-    #     print(p)
-    print(cky.predit(b))
+    with codecs.open(sys.argv[1], "r") as corpus:
+        for phrase in corpus:
+            if not phrase.startswith('('):
+                (nomcorpus_numero, phrase) = phrase.split('\t')
+                (nomcorpus, numero) = nomcorpus_numero.rpartition('_')[::2]
+            arbre=evaluation.readtree(evaluation.tokenize(phrase))[0]
+            phrase = evaluation.getleaves(arbre)
+            print("phrase_gold : ", phrase)
+            print(predit(phrase, cnf))
 
 
 if __name__ == '__main__':
