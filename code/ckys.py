48709c16b42a0a99d95dd8ee3164e2f74f8a0ca2
Krolov18 <korantin.leveque@gmail.com>
2016-06-17 22:51:54 +0200

    modify cry vers fonctionnel

--- code/ckys.py
+++ code/ckys.py
@@ -6,88 +6,88 @@ import fractions
 
 
 
-class CKY_TITOV(object):
-    def __init__(self):
-        self.chart = collections.defaultdict(lambda : collections.defaultdict(int))
 
-    def get_sent_sommet(self, phrase):
-        for x in self.grammaire['axiome']:
-            try:
-                sent_sommet = self.chart[(0, len(phrase))][x]
-            except KeyError:
-                print("La phrase n'est pas reconnue par la grammaire")
-            return (sent_sommet, (0, len(phrase)))
+def get_sent_sommet(phrase):
+    for x in self.grammaire['axiome']:
+        try:
+            sent_sommet = self.chart[(0, len(phrase))][x]
+        except KeyError:
+            print("La phrase n'est pas reconnue par la grammaire")
+        return (sent_sommet, (0, len(phrase)))
 
-    def predit(self, phrase, cnf):
-        print('Prédiction: ')
+def predit(phrase, cnf):
+    print('Prédiction: ')
 
-        chart = collections.defaultdict(lambda : collections.defaultdict(int))
+    chart = collections.defaultdict(lambda : collections.defaultdict(int))
 
-        return self.get_sommet(self.remplit(phrase, cnf, self.initialise(phrase, cnf)), phrase)
-        # return get_arbre(get_sommet(remplit(phrase, cnf, initialise(phrase, cnf, chart))))
+    return get_sommet(remplit(phrase, cnf, initialise(phrase, cnf)), phrase)
+    # return get_arbre(get_sommet(remplit(phrase, cnf, initialise(phrase, cnf, chart))))
 
-    def get_sommet(self, chart, phrase):
-        for x in list(chart[(len(phrase), 0)]):
-            if (x == "SENT") or "SENT" in x:
-                print(chart[(len(phrase), 0)][x])
+def get_sommet(chart, phrase):
+    for x in list(chart[(len(phrase), 0)]):
+        if (x == "SENT") or "SENT" in x:
+            print(chart[(len(phrase), 0)][x])
 
-    def get_arbre(self, sent_sommet):
-        pattern = '({tete} {enfants})'
-        tete = list(self.chart[sent_sommet[1]].keys())[0]
-        if (len(sent_sommet) == 1) and (isinstance(sent_sommet, (tuple, (int, int)))):
-            return pattern.format(tete=tete, enfants=tete.lower())
-        else:
-            if (len(sent_sommet) == 2) and (isinstance(sent_sommet, (tuple, (tuple, tuple)))):
-                return pattern.format(
-                    tete=tete,
-                    enfants=" ".join([self.get_arbre(child) for child in sent_sommet[1]])
-                )
+def get_arbre(sent_sommet):
+    pattern = '({tete} {enfants})'
+    tete = list(self.chart[sent_sommet[1]].keys())[0]
+    if (len(sent_sommet) == 1) and (isinstance(sent_sommet, (tuple, (int, int)))):
+        return pattern.format(tete=tete, enfants=tete.lower())
+    else:
+        if (len(sent_sommet) == 2) and (isinstance(sent_sommet, (tuple, (tuple, tuple)))):
+            return pattern.format(
+                tete=tete,
+                enfants=" ".join([self.get_arbre(child) for child in sent_sommet[1]])
+            )
 
-    def initialise(self, phrase, cnf):
-        print('Initialisation de la charte: ')
+def initialise(phrase, cnf):
+    print('Initialisation de la charte: ')
 
-        chart = collections.defaultdict(lambda: collections.defaultdict(int))
+    chart = collections.defaultdict(lambda: collections.defaultdict(int))
 
-        (terminaux, nonterminaux, regles) = cnf
+    (terminaux, nonterminaux, regles) = cnf
 
-        s = 1
+    s = 1
 
-        for (t, _) in enumerate(phrase):
-            for lhs in list(regles):
-                for rhs in list(regles.get(lhs)):
-                    if len(rhs) == 1:
-                        chart[(s, t)][lhs] = regles[lhs][rhs[0]]
-                        # chart[(s, t)][lhs] = (regles[lhs][rhs[0]], ((t, s),))
-        print('Chart initialisée')
-        return chart
+    for (t, _) in enumerate(phrase):
+        for lhs in list(regles):
+            for rhs in list(regles.get(lhs)):
+                if len(rhs) == 1:
+                    chart[(s, t)][lhs] = regles[lhs][rhs[0]]
+                    # chart[(s, t)][lhs] = (regles[lhs][rhs[0]], ((t, s),))
+    print('Chart initialisée')
+    return chart
 
-    def remplit(self, phrase, cnf, chart):
-        print('Remplissage de la charte: ')
 
-        span = (x for (x, y) in enumerate(phrase[1:], 2))
+def remplit(phrase, cnf, chart):
+    print('Remplissage de la charte: ')
 
-        (terminaux, nonterminaux, regles) = cnf
+    span = (x for (x, y) in enumerate(phrase[1:], 2))
 
-        for max in span:
-            for min in range(max-2, 0, -1):
-                print(min, max)
-                for nonterminal in nonterminaux:
-                    best = 0
-                    for rhs in list(regles[nonterminal]):
-                        if len(rhs) == 2:
-                            for mid in range(min + 1, max - 1):
-                                t1 = chart[(min, mid)][rhs[0]]
-                                t2 = chart[(mid, max)][rhs[1]]
-                                candidate = t1 * t2 * regles[nonterminal][rhs]
-                                if candidate > best:
-                                    best = candidate
-                                    # best = (candidate, ((min, mid, rhs[0]), (mid, max, rhs[1])))
-                            chart[(min, max)][nonterminal] = best
-        print('Chart remplie')
-        return chart
+    (terminaux, nonterminaux, regles) = cnf
+
+    for max in span:
+        for min in range(max-2, 0, -1):
+            print(min, max)
+            for nonterminal in nonterminaux:
+                best = 0
+                for rhs in list(regles[nonterminal]):
+                    if len(rhs) == 2:
+                        for mid in range(min + 1, max - 1):
+                            t1 = chart[(min, mid)][rhs[0]]
+                            t2 = chart[(mid, max)][rhs[1]]
+                            candidate = t1 * t2 * regles[nonterminal][rhs]
+                            if candidate > best:
+                                best = candidate
+                                # best = (candidate, ((min, mid, rhs[0]), (mid, max, rhs[1])))
+                        chart[(min, max)][nonterminal] = best
+    print('Chart remplie')
+    return chart
+
 
 def defaultdictmaker():
     return collections.defaultdict(fractions.Fraction)
+
 
 def main():
     import pickle
